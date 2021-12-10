5301499fa4f02a1ae8d6cbdce18da9fff305ac95
mrhrifat <mrhrifat383759@gmail.com>
2021-12-11 02:32:53 +0600

    OOP class for convesion

---
+++ src/components/Conversion.js
@@ -0,0 +1,46 @@
+const roman = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'L',
+    60: 'LX',
+    70: 'LXX',
+    80: 'LXXX',
+    90: 'XC',
+    100: 'C',
+    200: 'CC',
+    300: 'CCC',
+    400: 'CD',
+    500: 'D',
+    600: 'DC',
+    700: 'DCC',
+    800: 'DCCC',
+    900: 'CM',
+    1000: 'M',
+    2000: 'MM',
+    3000: 'MMM'
+
+}
+export default class Conversion {
+    convert(natural) {
+        let str = String(natural).split('')
+        let result = ''
+        for (let i = 0; i < str.length; i++) {
+            var lookup = str[i] * Math.pow(10, str.length - i - 1)
+            if (roman[lookup]) {
+                result += roman[lookup]
+            }
+        }
+        return result
+    }
+}
