d1cb67a7c4e86b76e38682c8c9b340a3da08d7e6
mrhrifat <mrhrifat383759@gmail.com>
2021-12-11 02:32:27 +0600

    Make update with OOP concept

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import Conversion from './Conversion'
+
+const con = new Conversion();
 
 class Root extends Component {
     state = {
         natural: '',
         roman: ''
+    }
+
+    componentDidMount() {
+        con.convert(this.state.natural)
     }
 
     handleChange = e => {
@@ -16,56 +23,56 @@ class Root extends Component {
         e.preventDefault()
 
         this.setState({
-            roman: this.convert(this.state.natural)
+            roman: con.convert((this.state.natural))
         })
-        console.log(this.convert(this.state.natural))
+        // console.log(this.convert(this.state.natural))
         // console.log(this.state.roman)
     }
 
-    convert = natural => {
-        const roman = {
-            1: 'I',
-            2: 'II',
-            3: 'III',
-            4: 'IV',
-            5: 'V',
-            6: 'VI',
-            7: 'VII',
-            8: 'VIII',
-            9: 'IX',
-            10: 'X',
-            20: 'XX',
-            30: 'XXX',
-            40: 'XL',
-            50: 'L',
-            60: 'LX',
-            70: 'LXX',
-            80: 'LXXX',
-            90: 'XC',
-            100: 'C',
-            200: 'CC',
-            300: 'CCC',
-            400: 'CD',
-            500: 'D',
-            600: 'DC',
-            700: 'DCC',
-            800: 'DCCC',
-            900: 'CM',
-            1000: 'M',
-            2000: 'MM',
-            3000: 'MMM'
+    // convert = natural => {
+    //     const roman = {
+    //         1: 'I',
+    //         2: 'II',
+    //         3: 'III',
+    //         4: 'IV',
+    //         5: 'V',
+    //         6: 'VI',
+    //         7: 'VII',
+    //         8: 'VIII',
+    //         9: 'IX',
+    //         10: 'X',
+    //         20: 'XX',
+    //         30: 'XXX',
+    //         40: 'XL',
+    //         50: 'L',
+    //         60: 'LX',
+    //         70: 'LXX',
+    //         80: 'LXXX',
+    //         90: 'XC',
+    //         100: 'C',
+    //         200: 'CC',
+    //         300: 'CCC',
+    //         400: 'CD',
+    //         500: 'D',
+    //         600: 'DC',
+    //         700: 'DCC',
+    //         800: 'DCCC',
+    //         900: 'CM',
+    //         1000: 'M',
+    //         2000: 'MM',
+    //         3000: 'MMM'
 
-        }
-        let str = String(natural).split('')
-        let result = ''
-        for (let i = 0; i < str.length; i++) {
-            var lookup = str[i] * Math.pow(10, str.length - i - 1)
-            if (roman[lookup]) {
-                result += roman[lookup]
-            }
-        }
-        return result
-    }
+    //     }
+    //     let str = String(natural).split('')
+    //     let result = ''
+    //     for (let i = 0; i < str.length; i++) {
+    //         var lookup = str[i] * Math.pow(10, str.length - i - 1)
+    //         if (roman[lookup]) {
+    //             result += roman[lookup]
+    //         }
+    //     }
+    //     return result
+    // }
 
     render() {
         console.log(this.state.natural)
@@ -78,7 +85,7 @@ class Root extends Component {
                         <div className='form-group'>
                             <label htmlFor='natural'>Enter natural number</label>
                             <input
-                                type="number"
+                                type="text"
                                 name="natural"
                                 id="natural"
                                 className='form-control'
@@ -86,16 +93,25 @@ class Root extends Component {
                                 onChange={this.handleChange}
                             />
                         </div>
+                        <div className='form-group my-2'>
+                            <input
+                                type="button"
+                                className='btn btn-sm btn-success'
+                                onClick={this.handleSubmit}
+                                value="Convert"
+                            />
+                        </div>
                         <div className="form-group">
                             <label htmlFor='roman'>Coverted roman numeral</label>
-                            <p>{roman}</p>
+                            <input
+                                disabled
+                                type="text"
+                                name="roman"
+                                id="roman"
+                                className='form-control'
+                                value={roman}
+                            />
                         </div>
-                        <input
-                            type="button"
-                            className='btn btn-sm btn-success'
-                            onClick={this.handleSubmit}
-                            value="Convert"
-                        />
                     </form>
                 </div>
             </div>
